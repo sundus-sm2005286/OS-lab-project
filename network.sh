b20a65f368e98247bf925903af4dea38c0cba51e
noof-na2002196 <na2002196@qu.edu.qa>
2024-12-01 19:49:47 +0300

    Server

--- network.sh
+++ network.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-sudo apt-get install net-tools
+
+is_valid_ip() {
+    local ip=$1
+    if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        local IFS="."
+        local -a octets
+        read -r -a octets <<< "$ip"
+        for octet in "${octets[@]}"; do
+            if (( octet < 0 || octet > 255 )); then
+                return 1  # Invalid IP address
+            fi
+        done
+        return 0  
+    else
+        return 1 
+    fi
+}
+
+target=$1
 
 
-target=$1 
+if ! is_valid_ip "$target"; then
+    echo "Invalid IP address format: $target"
+    exit 1
+fi
 
-
-ping -c 3 -W 3 $target #time out 3
-if [ $? -eq 0 ]
-
-then
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
-echo $current_date >> network.log
-echo "Connectivity of $target was successfully" | tee -a network.log
+# pinging and traceroute
+ping -c 3 -W 3 $target
+if [ $? -eq 0 ]; then
+    current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    echo $current_date >> network.log
+    echo "Connectivity with $target is ok" | tee -a network.log
 else
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
-echo $current_date >> network.log
-echo "Target: $target is not responding. Running traceroute.sh" 
-./traceroute.sh $target
-fi 
+    current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    echo $current_date >> network.log
+    echo "Target: $target is not responding. Running traceroute.sh" 
+    ./traceroute.sh $target
+fi
--- traceroute.sh
+++ traceroute.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install traceroute
+
+log_status() {
+    current_date=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$current_date - $1" | tee -a network.log
+}
 
 
-target=$1
+TARGET=$1
 
-echo "Running traceroute for $target " | tee -a network.log
-traceroute "$target" | tee -a network.log
+
+log_status "Displaying routing table"
+netstat -rn | tee -a network.log
+
+
+log_status "Displaying hostname"
+hostname | tee -a network.log
+
+
+log_status "Testing local DNS server"
+nslookup google.com | tee -a network.log
+
+# Traceroute to google.com
+log_status "Tracing route to google.com"
+traceroute google.com | tee -a network.log
+
+# Ping google.com , check internet connection
+log_status "Pinging google.com"
+ping -c 3 google.com | tee -a network.log
